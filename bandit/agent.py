31986c7b576c471baf8f33837976348f64ee85e2
Wilco Schoneveld <schoneveld.wj@gmail.com>
2017-09-25 17:01:27 +0200

    significant performance increase in value estimation

--- bandit/agent.py
+++ bandit/agent.py
@@ -7,32 +7,40 @@ class Agent(object):
         self.slots = slots
         self.greedy = greedy
 
-        self.history = {
-            'actions': [],
-            'rewards': []
-        }
+        self.estimates = {}
+
+        for slot in slots:
+            self.estimates[slot] = {
+                'chosen': 0,
+                'value': 0
+            }
+
+    def set_value(self, slot, value):
+        self.estimates[slot]['value'] = value
+
+    def get_value(self, slot):
+        return self.estimates[slot]['value']
+
+    def inc_steps(self, slot):
+        self.estimates[slot]['chosen'] += 1
+
+    def get_steps(self, slot):
+        return self.estimates[slot]['chosen']
 
     def draw(self):
         if random.random() < self.greedy:
-            slot = max(self.slots, key=lambda s: self.estimated_value(s))
+            slot = max(self.slots, key=self.get_value)
         else:
             slot = random.choice(self.slots)
 
-        value = slot.draw()
+        self.inc_steps(slot)
 
-        self.history['actions'].append(slot)
-        self.history['rewards'].append(value)
+        reward = slot.draw()
 
-        return value
+        old_value = self.get_value(slot)
+        new_value = old_value + (1 / self.get_steps(slot)) * (reward - old_value)
 
-    def estimated_value(self, slot):
-        values = []
+        self.set_value(slot, new_value)
 
-        for action, reward in zip(self.history['actions'], self.history['rewards']):
-            if action == slot:
-                values.append(reward)
+        return reward
 
-        if not values:
-            return 0
-
-        return sum(values) / len(values)
